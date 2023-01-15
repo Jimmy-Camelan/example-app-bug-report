5d36dac38587d0b539c97ea7edd402dcb0edb151
Jimmy88 <72346002+Jimmy-Camelan@users.noreply.github.com>
2023-01-15 21:32:17 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,16 +6,16 @@ html_string = """
   <source src="https://www.orangefreesounds.com/wp-content/uploads/2022/04/Small-bell-ringing-short-sound-effect.mp3" type="audio/mpeg">
   Your browser does not support the audio element.
 </audio>
+<script type="application/javascript">
+const myTimeout = setTimeout(playAudio, 5000);
+
+function playAudio() {
+  document.getElementById('audio').play()
+  clearTimeout(myTimeout);
+}
+</script>
             """
 
 sound = st.empty()
 sound.markdown(html_string, unsafe_allow_html=True)  # will display a st.audio with the sound you specified in the "src" of the html_string and autoplay it
-time.sleep(2)  # wait for 2 seconds to finish the playing of the audio
-sound_player = st.empty()
-html_string = """
-            <script type="application/javascript">
-  document.getElementById("audio").play();
-  console.log("It's play time!!");
-</script>
-            """
-sound_player.markdown(html_string, unsafe_allow_html=True)
+time.sleep(2)
